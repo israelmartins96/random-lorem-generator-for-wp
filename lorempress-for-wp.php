5c672302680f7187835e7b13fbc124b984544057
Israel Martins <m.oisrael96@gmail.com>
2024-11-27 11:33:12 +0100

    Added Pre-requisite Plugin Files

---
+++ lorempress-for-wp.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * LoremPress for WP
+ *
+ * @package             LoremPressForWP
+ * @author              Israel Martins
+ * @copyright           Copyright (c) 2024, Lightbulb Devs
+ * @license             GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:         LoremPress for WP
+ * Plugin URI:          https://israelmartins.com
+ * Description:         Add dummy text as a placeholder while creating your WordPress website, web page, blog post etc.
+ * Version:             0.1.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Author:              Israel Martins
+ * Author URI:          https://israelmartins.com
+ * Donate link:         https://israelmartins.com/donate/
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:          https://israelmartins.com
+ * Text Domain:         lorempress-for-wp
+ * Domain Path:         /languages/
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2024 Lightbulb Devs
+ */
