5eec784fedae77f8b13f54510605514426abacb3
Israel Martins <m.oisrael96@gmail.com>
2025-09-04 15:52:28 +0100

    feat(i18n): Add the i18n File, Header, and Class

---
+++ includes/base/i18n.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Define the internationalization functionality.
+ * 
+ * Loads and defines the internationalisation files for this plugin
+ * so that it is ready for translation.
+ * 
+ * @since 1.0.0
+ * 
+ * @package Random_Lorem_Generator
+ * @subpackage Random_Lorem_Generator/Includes
+ * @version 1.0.0
+ */
+
+namespace Random_Lorem_Generator\Includes\Base;
+
+/**
+ * Define the internationalization functionality.
+ * 
+ * Loads and defines the internationalisation files for this plugin
+ * so that it is ready for translation.
+ * 
+ * @since 1.0.0
+ * @package Random_Lorem_Generator
+ * @subpackage Random_Lorem_Generator/Includes
+ * @author Lightbulb Devs <dev@lightbulbdevs.com>
+ */
+
+class i18n {
+
+    
+    
+}
